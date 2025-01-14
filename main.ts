940bf71c056b58c0840b44cdf7e60f8f4ce7a9f0
maryam-ergen <maryam.ergen@mths.ca>
2025-01-14 10:20:25 -0500

    made code

--- main.ts
+++ main.ts
@@ -1,8 +1,21 @@
-/* Copyright (c) 2020 MTHS All rights reserved
+ +/* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Maryam Ergen and Ella An
+ * Created on: Nov 14
+ * This program is a pizza truck
 */
 
-basic.showString('Hello, World!')
+// setup
+radio.setGroup(131)
+
+//to start going...ballalala
+radio.onReceivedNumber(function (receivedNumber) {
+    basic.showIcon(IconNames.Heart)
+    if (receivedNumber == 1) {
+    robotbit.StpCarMove(10, 48)
+    }
+    else  {
+        robotbit.StpCarMove(-10, 48)
+    }
+
+    })
